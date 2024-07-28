06d56a0cfecb296fe92de4177e93d123d276e038
queerio16 <asrms2006@gmail.com>
2024-07-28 22:21:46 +1200

    Update my_wallpaper.js
    trying to figure out what spacing i want between each turtle. currently thinking 150

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -10,7 +10,7 @@ function setup_wallpaper(pWallpaper) {
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 250;
+  pWallpaper.grid_settings.cell_height = 400;
   pWallpaper.grid_settings.row_offset  = 0;
 }
 
@@ -47,27 +47,45 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   bezier(35,54,20,20,80,20,65,54) //head
   bezier(50,150,-10,85,25,50,50,50) //left body left
   bezier(50,150,110,85,75,50,50,50) //left body right
-  //line(0,75,200,75)
   bezier(71,122,87,128,77,152,60,138) //flipper left
+  bezier(29,122,13,128,23,152,40,138) //flipper right
+  //line(0,75,200,75)
   //line(0,122,200,122)
   //line(0,138,200,138)
   //line(29,0,29,200)
   //line(40,0,40,200)
-  bezier(29,122,13,128,23,152,40,138) //flipper right
+
   //line(0,143,200,143)
  // line(0,75,200,75)
 
+
   //flipped turtle
   //bezier(35,54,20,20,80,20,65,54) //head
-  bezier(135,146,120,180,180,180,165,146)
+ /* bezier(135,146,120,180,180,180,165,146)
   bezier(150,50,90,115,125,150,150,150) //right body left
   bezier(150,50,210,115,175,150,150,150) //right body right
  
   //line(0,62,200,62)
   //line(0,78,200,78)
-  bezier(129,78,113,72,123,48,140,62)
-  bezier(171,78,187,72,177,48,160,62)
-
-
+  bezier(129,78,113,72,123,48,140,62) //right flipper left
+  bezier(171,78,187,72,177,48,160,62) //right fliper right
+*/
   
+  //flipped turtle offset 
+  bezier(135,196,120,230,180,230,165,196)
+  bezier(150,100,90,165,125,200,150,200)
+  bezier(150,100,210,165,175,200,150,200)
+  bezier(129,128,113,122,123,98,140,112)
+  bezier(171,128,187,122,177,98,160,112)
+
+  // agian!
+  bezier(135,346,120,380,180,380,165,346)
+  bezier(150,250,90,315,125,350,150,350)
+  bezier(150,250,210,315,175,350,150,350)
+  bezier(129,278,113,272,123,248,140,262)
+  bezier(171,278,187,272,177,248,160,262)
+
+  // a third time!
+
+
 }
