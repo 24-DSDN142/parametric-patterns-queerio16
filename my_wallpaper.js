5c6b307aceb281c8c358ad740e7c994105ad3bd1
queerio16 <asrms2006@gmail.com>
2024-08-01 11:17:27 +1200

    Update my_wallpaper.js
    i made lots of progess and forgot to upload oops. Ive made my if statement change the background colour to purple if a flipper gets too big. ive also got shell_colour working, and all the arms done :D

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,10 @@
 //your parameter variables go here!
-let shift_right = (25); // could use a maths var?
-let flip_size = (100);
-let shell_colour = (80, 141, 78)
+let shell_colour;
+let shift_right = (35); // could use a maths var?
+let flip_size = (35);
+let eye = (5);
+
+
 
 // shell shape/line variable
 // colour
@@ -23,21 +26,24 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 300;  // so much less work to have it at 300 instead of 400. few
   pWallpaper.grid_settings.row_offset  = 0;
+  shell_colour = color(80,141,78);
 }
 
 function wallpaper_background() {
   background(187, 233, 255); //light honeydew green colour
-}
+
+  if (flip_size > 30) 
+    background(212, 184, 255)}
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //turtle attempt 2 :)
-
+  
   //colouring
   fill(79, 121, 66)
   noStroke()
 
   bezier(35,54,20,20,80,20,65,54) //head
-  fill(80, 141, 78)
+  fill(shell_colour)
   bezier(50,150,-10,85,25,50,50,50) //left body left
   bezier(50,150,110,85,75,50,50,50) //left body right
   fill(79, 121, 66)
@@ -55,7 +61,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   
   //flipped turtle offset 
   bezier(135,196,120,230,180,230,165,196)
-  fill(80, 141, 78)
+  fill(shell_colour)
   bezier(150,100,90,165,125,200,150,200)
   bezier(150,100,210,165,175,200,150,200)
   fill(79, 121, 66)
@@ -64,7 +70,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   // agian!
   bezier(135,346,120,380,180,380,165,346)
-  fill(80, 141, 78)
+  fill(shell_colour)
   bezier(150,250,90,315,125,350,150,350)
   bezier(150,250,210,315,175,350,150,350)
   fill(79, 121, 66)
@@ -73,7 +79,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   // upright bottom turtle
   bezier(35,204,20,170,80,170,65,204) //head
-  fill(80, 141, 78)
+  fill(shell_colour)
   bezier(50,300,-10,235,25,200,50,200) //left body left
   bezier(50,300,110,235,75,200,50,200) //left body right
   fill(79, 121, 66)
@@ -84,13 +90,23 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   bezier(18,230,5,226,-5,204,27,210)
   bezier(28,59,5,52,-5,70,18,78)
 
-  bezier(71,59,94,52,104,70,81,78)
-  bezier(82,230,94,226,104,204,71,210)
+  bezier(71,59,94,52,104,70,82,78)
+  bezier(82,230,94,226,104,204,73,210)
 
-  //bezier(29,272,13,278,23,302,40,288) 
-//upright bottom flipper left reference
-  //bezier(29,122,13,128,23,152,40,138)
-//top left turtle flipper left
-  //bezier(129-10,122+50,113-23,128+50,123-23,152+50,140-13,138+50)
-  
+  bezier(118,170,98,181,103,202,128,190)
+  bezier(118+64,170,98+100,177,103+100,200,128+45,190)
+
+  bezier(118,170+150,98-(flip_size),181+150,103,202+150,128,190+150)
+  bezier(118+64,170+150,98+100+(flip_size),177+150,103+100,200+150,128+45,190+150)
+
+  //eye??
+  fill(0)
+  circle(41,38,eye,eye)
+  circle(59,38,eye,eye)
+  circle(41,38+150,eye,eye)
+  circle(59,38+150,eye,eye)
+  circle(41+100,38+24,eye,eye)
+  circle(59+100,38+24,eye,eye)
+
+
 }
